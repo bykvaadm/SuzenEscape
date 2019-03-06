0af4e37059993326d135cd291c40346d7ce50dc8
Alexander Kondratyev <akondratyev@wallarm.com>
2019-03-07 00:15:45 +0300

    Huge levels refactoring
    - move all tasks to chain subdir
    - migrate all tasks to own dockerfiles
    - remove trash

--- build.sh
+++ build.sh
@@ -10,25 +10,18 @@ fi
 
 level="$1"
 echo "building level $1"
-#source level${level}/values
 
 FLAG=$(cat ansible/vars.yaml | ruby -ryaml -e "puts YAML.load($<)[\"levels\"].select{|x| x[\"name\"] == \"suzen${level}\"}.map{|y| y[\"flag\"]}")
 NAME=$(cat ansible/vars.yaml | ruby -ryaml -e "puts YAML.load($<)[\"levels\"].select{|x| x[\"name\"] == \"suzen${level}\"}.map{|y| y[\"name\"]}")
 CONFIG=$(cat ansible/vars.yaml | ruby -ryaml -e "puts YAML.load($<)[\"levels\"].select{|x| x[\"name\"] == \"suzen${level}\"}.map{|y| y[\"config\"]}")
 VERSION=$(cat ansible/vars.yaml | ruby -ryaml -e "puts YAML.load($<)[\"levels\"].select{|x| x[\"name\"] == \"suzen${level}\"}.map{|y| y[\"version\"]}")
 ROHOME=$(cat ansible/vars.yaml | ruby -ryaml -e "puts YAML.load($<)[\"levels\"].select{|x| x[\"name\"] == \"suzen${level}\"}.map{|y| y[\"rohome\"]}")
+CHAIN=$(cat ansible/vars.yaml | ruby -ryaml -e "puts YAML.load($<)[\"levels\"].select{|x| x[\"name\"] == \"suzen${level}\"}.map{|y| y[\"chain\"]}")
 
 if [ -z ${FLAG+x} ] || [ "${FLAG}" == "" ]; then FLAG="NONE"; fi
 if [ -z ${CONFIG+x} ] || [ "${CONFIG}" == "" ]; then CONFIG="NONE"; fi
 if [ -z ${VERSION+x} ] || [ "${VERSION}" == "" ]; then VERSION="NONE"; fi
 if [ -z ${ROHOME+x} ] || [ "${ROHOME}" == "" ]; then ROHOME="false"; fi
-
-echo $NAME
-echo $CONFIG
-echo $VERSION
-echo $ROHOME
-
-#exit 0
 
 if [ -z ${ROHOME+x} ] || [ ${ROHOME} == "false" ]; then
   USERHOME="$NAME"
@@ -36,47 +29,10 @@ else
   USERHOME="root"
 fi
 
-echo $USERHOME
-
-# TO DO: filter out if CONFIG and SYSCONFIG have same binaries
-# sysconfig - system utils for building
-SYSCONFIG="chown|chmod|adduser|addgroup|egrep|xargs|rm|ls"
-# level utils + build utils
-JOINCONFIG="${CONFIG}|${SYSCONFIG}"
-#export CONFIG="$CONFIG"
-#echo "${CONFIG}"
-
-if [ -e level${level}/Dockerfile ]; then
-  cd level${level}/
-  docker build -t ${REGISTRY_URL}/suzenescape/${NAME} . \
-  --build-arg USERNAME=${NAME} \
-  --build-arg CONFIG=${CONFIG} \
-  --build-arg USERHOME=${USERHOME} \
-  --build-arg FLAG=${FLAG}
-  docker push ${REGISTRY_URL}/suzenescape/${NAME}
-else
-  # copy and extract busybox layer
-  cp busybox/layer.tar.gz ./ && mkdir layer && tar zxvf layer.tar.gz -C layer 1>/dev/null && rm layer.tar.gz
-
-  # rm all unnescesary stuff from busybox
-  cd layer/bin && ls | egrep -vw "(${JOINCONFIG})" | xargs rm && ls
-
-  # archive new busybox layer and move it to Dockerfile
-  cd ../ && pwd && tar zcvf layer.tar.gz ./* && mv layer.tar.gz ../
-
-  # return upper and cleanup busybox layer
-  cd ../ && rm -rf ./layer
-
-  # archive game layer and move it to dockerfile
-  cd level${level}/layer && tar zcvf level.tar.gz ./* && mv level.tar.gz ../../ && cd ../../
-
-  # build and push image
-  docker build -t ${REGISTRY_URL}/suzenescape/${NAME} . \
-  --build-arg USERNAME=${NAME} \
-  --build-arg CONFIG=${CONFIG} \
-  --build-arg USERHOME=${USERHOME}
-  docker push ${REGISTRY_URL}/suzenescape/${NAME}
-
-  # cleanup
-  rm layer.tar.gz level.tar.gz
-fi
+cd chain${CHAIN}/level${level}/
+docker build -t ${REGISTRY_URL}/suzenescape/${NAME} . \
+--build-arg USERNAME=${NAME} \
+--build-arg CONFIG=${CONFIG} \
+--build-arg USERHOME=${USERHOME} \
+--build-arg FLAG=${FLAG}
+docker push ${REGISTRY_URL}/suzenescape/${NAME}
