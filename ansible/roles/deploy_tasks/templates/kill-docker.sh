19793a05126071178993701c9702926a4619b4c5
Alexander Kondratyev <a.kondratev@city-mobil.ru>
2021-06-15 16:02:43 +0300

    add ability to write custom kill docker regexp

--- ansible/roles/deploy_tasks/templates/kill-docker.sh
+++ ansible/roles/deploy_tasks/templates/kill-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker ps --format='{{.ID}}' | \
   xargs -n 1 -r docker inspect -f '{{.ID}} {{.State.Running}} {{.State.StartedAt}} {{.Name}}' | \
-  egrep -v '(ctfchecker|web|registry|rabbitmq)' | \
+  egrep -v '{{ deploy_tasks_kill_docker_regexp }}' | \
   awk '$2 == "true" && $3 <= "'$(date -d '30 minutes ago' -Ins --utc | sed 's/+00:00/Z/')'" {print $1}' | \
   xargs -r docker kill && \
   docker system prune -f
