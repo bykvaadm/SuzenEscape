46de7753059586b1b4973995ff355e15a5c54c1f
Alexander Kondratyev <a.kondratev@city-mobil.ru>
2021-06-16 17:46:18 +0300

    fix kill-docker

--- ansible/roles/deploy_tasks/templates/kill-docker.sh
+++ ansible/roles/deploy_tasks/templates/kill-docker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+{% raw %}
 docker ps --format='{{.ID}}' | \
   xargs -n 1 -r docker inspect -f '{{.ID}} {{.State.Running}} {{.State.StartedAt}} {{.Name}}' | \
+{% endraw %}
   grep suzen | egrep -v '{{ deploy_tasks_kill_docker_regexp }}' | \
   awk '$2 == "true" && $3 <= "'$(date -d '30 minutes ago' -Ins --utc | sed 's/+00:00/Z/')'" {print $1}' | \
   xargs -r docker kill && \
